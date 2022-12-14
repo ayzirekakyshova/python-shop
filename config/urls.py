c8f264caff94b588e59f4d21dea2082bef9e6ad6
acle <ayzirekakyshova04@gmail.com>
2022-12-14 10:56:30 +0600

    images

--- config/urls.py
+++ config/urls.py
@@ -38,3 +38,11 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include('review.urls')),
 ]
+
+""" подключение media и static"""
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
