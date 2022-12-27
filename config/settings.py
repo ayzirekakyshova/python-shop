5522554c9758ea288842fc2758fcb8b99f7a5d55
acle <ayzirekakyshova04@gmail.com>
2022-12-27 11:46:49 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.118.19.184', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'account',
     'main',
     'review',
+
 ]
 
 MIDDLEWARE = [
